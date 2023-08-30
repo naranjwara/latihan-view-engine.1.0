b2c35231584bd7503a999c0935a3c5f27e6d6b16
Nabilah Aysyah <nabilahnajwa808@gmail.com>
2023-08-30 13:37:41 +0700

    result akhir dari latihan view engine 1.0

--- app1.js
+++ app1.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
@@ -20,16 +21,20 @@ app.get('/', (req, res) => {
 });
 
 // Tambahkan item ke keranjang
-app.get('/add-to-cart/:id', (req, res) => {
-  const productId = parseInt(req.params.id);
-  const product = products.find(item => item.id === productId);
-
-  if (product) {
-    cartItems.push(product);
-  }
-
-  res.redirect('/');
-});
+app.post('/add-to-cart', (req, res) => {
+    const productName = req.body.productName;
+    const quantity = parseInt(req.body.quantity) || 1; // Mengambil jumlah, default 1
+    const product = products.find(item => item.name === productName);
+  
+    if (product) {
+      // Menambahkan item dengan jumlah ke keranjang
+      for (let i = 0; i < quantity; i++) {
+        cartItems.push(product);
+      }
+    }
+  
+    res.redirect('/');
+  });
 
 // Hitung total harga di keranjang
 function calculateTotal(cart) {
