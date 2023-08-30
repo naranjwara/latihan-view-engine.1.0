dbc9df0f399df1c1ebe9648e7b36400f2f55d96b
Nabilah Aysyah <nabilahnajwa808@gmail.com>
2023-08-30 13:07:43 +0700

    menambahkan fitur input harga

--- app1.js
+++ app1.js
@@ -7,12 +7,14 @@ app.use(express.static('public'));
 
 // Data simulasi daftar produk
 const products = [
-  { id: 1, name: 'Product A', price: 10 },
-  { id: 2, name: 'Product B', price: 20 },
-  { id: 3, name: 'Product C', price: 30 },
+  { id: 1, name: 'Product A', price: 0 },
+  { id: 2, name: 'Product B', price: 0 },
+  { id: 3, name: 'Product C', price: 0 },
 ];
 
 let cartItems = []; // Simpan item keranjang
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     const total = calculateTotal(cartItems);
@@ -20,16 +22,22 @@ app.get('/', (req, res) => {
 });
 
 // Tambahkan item ke keranjang
+// Tambahkan item ke keranjang
 app.get('/add-to-cart/:id', (req, res) => {
-  const productId = parseInt(req.params.id);
-  const product = products.find(item => item.id === productId);
-
-  if (product) {
-    cartItems.push(product);
-  }
-
-  res.redirect('/');
-});
+    const productId = parseInt(req.params.id);
+    const quantity = parseInt(req.query.quantity) || 1;
+    const price = parseFloat(req.query.price) || 0.01; // Mengambil harga, default 0.01
+    const product = products.find(item => item.id === productId);
+  
+    if (product) {
+      // Menambahkan item dengan jumlah dan harga ke keranjang
+      for (let i = 0; i < quantity; i++) {
+        cartItems.push({ ...product, price }); // Menambahkan harga ke item
+      }
+    }
+  
+    res.redirect('/');
+  });
 
 // Hitung total harga di keranjang
 function calculateTotal(cart) {
